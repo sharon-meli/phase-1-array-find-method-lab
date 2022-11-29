615516d64a5a2d19baaf42f55e907d4fef268d06
sharon <sharonchepkirui665@gmail.com>
2022-11-30 01:08:51 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // code your solution here
+function superbowlWin(record){
+    let win=record.find(record=>record.result==="W")
+    if(win){
+        return win.year
+    }
+    else{
+        return undefined
+    }
+}
